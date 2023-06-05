32304eefe1c5d101d183e9b078d7645038bbec07
HoneyLee <HoneyLee200046@gmail.com>
2023-06-06 05:14:30 +0800

    fix: discord config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: "mongodb://localhost:27017/mern",
-    secretOrKey: "FxUum76z"
+    secretOrKey: "FxUum76z",
+    discordbot:""
 };
--- utils/discordbot.js
+++ utils/discordbot.js
@@ -3,6 +3,7 @@
   const Discord = require('discord.js')
   const client = new Discord.Client();
   const Verified = require("../models/Verified");
+  const config = require("../config/keys");
   // Register an event so that when the bot is ready, it will log a messsage to the terminal
   client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -72,7 +73,7 @@
 
   // client.login logs the bot in and sets it up for use. You'll enter your token here.
 // client.login logs the bot in and sets it up for use. You'll enter your token here.
-client.login('MTExNTMyMzQyOTQ3MDYwOTUwOA.GsIJox.JzeD9bgk0wJLlsdy2Elp-KehqnktnII9qy4kuE');
+client.login(config.discordbot);
 
 
 async function checkNFTowner(ownerAddress) {
