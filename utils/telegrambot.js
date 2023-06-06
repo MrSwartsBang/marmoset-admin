74a9b7a52fafe60f921da19b7e779acad07ceb6b
HoneyLee <HoneyLee200046@gmail.com>
2023-06-07 05:30:08 +0800

    fix:telegram bot

--- utils/telegrambot.js
+++ utils/telegrambot.js
@@ -1,5 +1,7 @@
 const TeleBot = require("telebot");
 const config = require("../config/keys");
+const { clientAPI, APICall } = require("./getNFT");
+const Verified = require("../models/Verified");
 // const { Telegraf } = require('telegraf');
 
 // const bot = new Telegraf(config.telegram);
@@ -31,11 +33,56 @@ const bot = new TelegramBot(config.telegram, { polling: true });
 // Replace CHANNEL_NAME_HERE with the name of your channel (including @ symbol)
 const channelName = 'CHANNEL_NAME_HERE';
 
-bot.on('message', (msg) => {
+bot.on('message',async (msg) => {
   const chatId = msg.chat.id;
-  console.log(msg);
-  // Check if message was sent in the channel
-  if (msg.chat.username === channelName.substr(1)) {
-    bot.sendMessage(chatId, 'Hello');
+  console.log(msg.from.username);
+  const isVerifiedUser = await Verified.findOne({discord:msg.from.username});
+  if(isVerifiedUser){
+    const NFTcount = await checkNFTowner(isVerifiedUser.wallet);
+    bot.sendMessage(chatId,"You own "+NFTcount+" NFTs.");
+  }else{
+    bot.sendMessage(chatId,"You are not a member of marmoset, please verify. http://ec2-44-201-124-72.compute-1.amazonaws.com/verify")
   }
+  // Check if message was sent in the channel
+  // if (msg.chat.username === channelName.substr(1)) {
+  //   bot.sendMessage(chatId, 'Hello');
+  // }
 });
+
+
+
+
+async function checkNFTowner(ownerAddress) {
+  console.log(clientAPI);
+  const allCollectionsOwned = await clientAPI("post", "/getCollections", {
+      limit: 10000,
+      offset: 0,
+      sort: -1,
+      isActive: true
+  });
+  let data = await Promise.all(
+      allCollectionsOwned?.map(async (collection) => {
+          const options = {
+              collection_address: collection.nftContractAddress,
+              owner: ownerAddress,
+              limit: 10000,
+              offset: 0,
+              sort: -1
+          };
+
+          let { ret: dataList } = await APICall.getNFTsByOwnerAndCollection(options);
+
+          dataList = dataList.filter((item) => item.is_for_sale !== true);
+          
+          const data = dataList?.map((item) => {
+              return { ...item, stakeStatus: 0 };
+          });
+
+          collection.listNFT = data;
+
+          return collection;
+      })
+  );
+  data = data.filter((item) => item.listNFT?.length > 0);
+  return data[0].listNFT.length;
+}
