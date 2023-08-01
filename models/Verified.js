8c88f2c416d2b79435ed311e47c791c7a0c6ec2d
HoneyLee <HoneyLee200046@gmail.com>
2023-08-01 17:26:34 +0800

    fix: nftCount

--- models/Verified.js
+++ models/Verified.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
+const {checkNFTowner} = require("../utils/discordbot");
 const VerifiedSchema = new Schema({
     discord: {
         type: String
