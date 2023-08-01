af78fab40634dec48c2ae82a286fbe0be55d1275
HoneyLee <HoneyLee200046@gmail.com>
2023-08-01 16:41:54 +0800

    fix: nftCount

--- utils/discordbot.js
+++ utils/discordbot.js
@@ -120,6 +120,7 @@
 // client.login logs the bot in and sets it up for use. You'll enter your token here.
 
 async function checkNFTowner(ownerAddress) {
+  console.log("ownerAddress:",ownerAddress);
   const allCollectionsOwned = await clientAPI("post", "/getCollections", {
       limit: 10000,
       offset: 0,
