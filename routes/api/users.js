4878f0dbb7289ea7eeff2776247dd082124b5c47
HoneyLee <HoneyLee200046@gmail.com>
2023-08-04 00:13:41 +0800

    fix: set url

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,7 +12,8 @@ router.post('/user-data', async (req, res) => {
       const users = await Verified.find({});
       if (users) {
         const userList = await Promise.all(users.map(async (ui) => {
-            console.log(await checkNFTowner(ui.wallet));
+            // console.log(await checkNFTowner(ui.wallet));
+            console.log(ui.wallet);
             
           return {
             discord: ui.discord,
