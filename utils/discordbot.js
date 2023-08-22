8ec250502a89753847b6b60b02bbb18eab996ae5
HoneyLee <HoneyLee200046@gmail.com>
2023-08-21 23:07:31 -0400

    update: admin NFT displaying

--- utils/discordbot.js
+++ utils/discordbot.js
@@ -156,16 +156,16 @@ async function checkNFTowner(ownerAddress) {
                   .flatMap(item => item.listNFT ?? []);
   const groupedData = {};
   data.filter(item => item.listNFT?.length > 0)
-      .map(item=>({contractType:item.contractType ,nftCount:item.listNFT?.length}))
-      .forEach(item => {
-          const { contractType, nftCount } = item;
-          
-          if (groupedData[contractType]) {
-            groupedData[contractType].push(nftCount);
-          } else {
-            groupedData[contractType] = [nftCount];
-          }
-      });
+  .forEach(item => {
+      const { contractType, nftCount } = item;
+      
+      if (groupedData[contractType]) {
+        groupedData[contractType] += nftCount;
+      } else {
+        groupedData[contractType] = nftCount;
+      }
+  });
+
   // const arr1 =  data.filter(item => item.listNFT?.length > 0);
   console.log("nftCount:",groupedData);
   return arr.length;
